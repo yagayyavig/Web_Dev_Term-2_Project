209fed4ef3345cc416ddecb73c9bc77646c22e7e
yagayya vig <yagayyavig@gmail.com>
2025-03-26 12:42:20 -0800

    Show Post and create post in postRouter

--- routers/postRouters.ts
+++ routers/postRouters.ts
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router();
 import { ensureAuthenticated } from "../middleware/checkAuth";
-import { getPosts } from "../fake-db";
+import { getPosts, getPost, addPost, editPost, deletePost, addComment } from "../fake-db";
 
 router.get("/", async (req, res) => {
-  const posts = await getPosts(20);
-  const user = await req.user;
+  const posts = getPosts(20);
+  const user = req.user;
   res.render("posts", { posts, user });
 });
 
@@ -13,37 +13,75 @@ router.get("/create", ensureAuthenticated, (req, res) => {
   res.render("createPosts");
 });
 
+// Yagayya 
 router.post("/create", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  try {
+    const { title, link, creator, description, subgroup } = req.body;
+
+    if (!title || !link || !creator || !description || !subgroup) {
+      return res.render("createPosts", {
+        error: "All fields are required",
+        formData: req.body,
+        user: req.user,
+      });
+    }
+
+    addPost(title, link, creator, description, subgroup);
+    res.redirect("/posts");
+  } catch (err) {
+    console.error("There is an error", err);
+    res.render("createPosts", {
+      error: "Failed to create post. Please try again.",
+      formData: req.body,
+      user: req.user,
+    });
+  }
 });
 
+// yagayya
 router.get("/show/:postid", async (req, res) => {
-  // ⭐ TODO
-  res.render("individualPost");
+  try {
+    const postId = Number(req.params.postid);
+    const post = getPost(postId);
+
+    if (!post) {
+      return res.status(404).render("error", {
+        message: "Post Not found",
+        user: req.user,
+      });
+    }
+
+    res.render("individualPost", {
+      post,
+      user: req.user,
+    });
+  } catch (err) {
+    console.error("Error fetching post:", err);
+    res.status(500).render("error", {
+      message: "Unable to retrieve post",
+      user: req.user,
+    });
+  }
 });
 
 router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // TODO
 });
 
 router.post("/edit/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // TODO
 });
 
 router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // TODO
 });
 
 router.post("/delete/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // TODO
 });
 
-router.post(
-  "/comment-create/:postid",
-  ensureAuthenticated,
-  async (req, res) => {
-    // ⭐ TODO
-  }
-);
+router.post("/comment-create/:postid", ensureAuthenticated, async (req, res) => {
+  // TODO
+});
 
 export default router;
